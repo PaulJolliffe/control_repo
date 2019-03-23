0109f040a15b157ebd1df89aa9eaf6276ddfadeb
PaulJolliffe <42622618+PaulJolliffe@users.noreply.github.com>
2019-03-23 07:14:15 +0000

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
                 ensure => present,
                 user   => 'root',
                 type   => 'ssh-rsa',
-                key    => '',
+                key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC00gHPbrPKWm56fgEKWtDmnKPgZzxrDo4hK2gtsyY8GTwBeJ61e8fnrNGo3eJ8R9wA6T7uXTOtbqhdFv/FuektfTScP215DBeaFrTuj23HBOeE+AJL+ole7Th7pgZW/ysUAtZNjG3T9vIbIjJ7Mw5i0MZVJUOkgUlAfy5Kn8gXZUUQ0eTcN/wu6phSFJMIviDaVGh0OmRN+NuqlR5c9gp9vE1j6arhFm/Fg7XoO9xzBTyogaEpHrWrOAdXk1t+Sg9snF/0Gfg3jf5wM8YK9x5aH7ajlWSVdegsHPsvO8pV844BF8sUwb7158ny0+fQbIgTyQk+vRk4X4F8j5cgU3G1',
         }
 }
